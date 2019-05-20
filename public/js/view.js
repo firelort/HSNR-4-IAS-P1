46c71128117ebdf28da080f8d0fb779d7350b6d8
Alexander Niersmann <aniersmann@gmail.com>
2019-05-20 09:29:57 +0200

    added event handling to views

--- public/js/view.js
+++ public/js/view.js
@@ -3,6 +3,7 @@ class View {
     constructor(elementSelector, templatePath) {
         this.elementSelector = elementSelector;
         this.templatePath = templatePath;
+        this.listeners = [];
     }
 
     render(data, elementSelector, templatePath) {
@@ -25,5 +26,35 @@ class View {
         } else {
             console.log(this.constructor.name + ' hat keine notify-Funktion');
         }
+    }
+
+    handleEvents(event) {
+        if (this.listeners[event.type]) {
+            for (let selector in this.listeners[event.type]) {
+                let nodes = document.querySelectorAll(this.elementSelector + ' ' + selector);
+                if ([].indexOf.call(nodes, event.target) >= 0) {
+                    this.listeners[event.type][selector](event);
+                }
+            }
+
+        }
+    }
+
+    addEventListener(event, selector, callable) {
+
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+            let element = document.querySelector(this.elementSelector);
+            if (element != null) {
+                element.addEventListener(event, this.handleEvents.bind(this));
+            } else {
+                console.warn('Konnte EventListener nicht anwenden!');
+            }
+
+
+        }
+
+        this.listeners[event][selector] = callable;
+
     }
 }
